436b4edefc7e7904428ec3dbbf31bf53d2cb091c
Jeffrey Graves <jgraves@jgmac-2.local>
2010-04-17 14:37:40 -0500

    Files added

---
+++ src/com/onemorepoint/novacraft/game/Game.java
@@ -0,0 +1,39 @@
+package com.onemorepoint.novacraft.game;
+
+import java.lang.System;
+
+public class Game
+{
+	protected double elapsedTime;
+	
+	private long lastTime;
+	private long thisTime;	
+
+	public void Game()
+	{
+		elapsedTime = 0;
+		thisTime = System.nanoTime();
+	}
+
+	private void UpdateTime()
+	{
+		lastTime = thisTime;
+		thisTime = System.nanoTime();
+		elapsedTime = (double)(thisTime - lastTime) * 0.000000001;
+	}
+		
+	public void Initialize()
+	{
+		
+	}
+	
+	public void Update()
+	{
+		UpdateTime();
+	}
+	
+	public void Render()
+	{
+		
+	}
+}
---
+++ src/com/onemorepoint/novacraft/novagame/PlayerShip.java
@@ -0,0 +1,26 @@
+package com.onemorepoint.novacraft.novagame;
+
+import com.onemorepoint.novacraft.game.*;
+
+public class PlayerShip extends GameObject
+{
+	
+	public void PlayerShip()
+	{
+
+	}
+
+    @Override
+	public void Update(float elapsedTime)
+	{
+        super.Update(elapsedTime);
+		
+	}
+	
+    @Override
+	public void Render(float elapsedTime)
+	{
+        super.Render(elapsedTime);
+		
+	}
+}
